6ae6397f3d61881541a76f9d6ebb8847bdbe0161
TRUSTMEIMDEAD <strinput@gmail.com>
2020-11-17 11:41:06 +0300

    still not a magician

---
+++ Camm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Camm : MonoBehaviour
+{
+
+    public GameObject plaer;
+    private Vector3 offset;
+
+
+    void Start()
+    {
+        offset = transform.position - plaer.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = plaer.transform.position + offset;
+    }
+
+}
---
+++ Rotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+
+public class Rotation : MonoBehaviour
+{
+    public int speed = 1;
+    public float speed2;
+    float hitdist = 0.0f;
+
+    void Update()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed2 * Time.deltaTime);
+            }
+        }
+
+
+
+    }
+}
